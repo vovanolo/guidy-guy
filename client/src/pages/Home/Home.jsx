8a7b73262aef6d2e53751b71d56bb8f044aa759b
vovanolo <vovchik.student221@gmail.com>
2021-02-24 16:25:14 +0200

    feat: installed icons and material core

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import styles from "./Home.module.css";
+import Navbar from "../../components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1 className={styles.homeTitle}>Home page</h1>
     </div>
   );
