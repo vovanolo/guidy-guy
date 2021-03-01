108754158ce9d5fb9df686855b02d73fa2e30249
vovanolo <vovchik.student221@gmail.com>
2021-03-01 13:26:25 +0200

    removed sidebar to map page

--- client/src/components/MapSidebar/MapSidebar.jsx
+++ client/src/components/MapSidebar/MapSidebar.jsx
@@ -1,59 +1,30 @@
-import styles from "./MapSidebar.module.css";
+// import styles from "./MapSidebar.module.css";
 
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Button from "@material-ui/core/Button";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Paper from "@material-ui/core/Paper";
+// import ButtonGroup from "@material-ui/core/ButtonGroup";
+// import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    marginTop: 20,
-    background: "#D7E1E9",
-    minHeight: 700,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
-}));
-
-export default function MapSidebar() {
-  const classes = useStyles();
-  const [categories, setCategories] = useState([]);
-
-  useEffect(async () => {
-    const res = await axios("https://alin-ua-api.herokuapp.com/categories");
-
-    setCategories(res.data);
-  }, []);
-
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Paper className={classes.paper}>
-          <ButtonGroup
-            orientation="vertical"
-            color="primary"
-            aria-label="vertical contained primary button group"
-            variant="text"
-          >
-            {categories.map((categori) => (
-              <Button key={categori.id}>{categori.title}</Button>
-            ))}
-          </ButtonGroup>
-        </Paper>
-      </React.Fragment>
-    );
-  }
-
-  return (
-    <div className={classes.root}>
-      <FormRow />
-    </div>
-  );
-}
+// export default function MapSidebar() {
+//   return (
+//     <div className={classes.root}>
+//       <React.Fragment>
+//         <Paper className={classes.paper}>
+//           <ButtonGroup
+//             orientation="vertical"
+//             color="primary"
+//             aria-label="vertical contained primary button group"
+//             variant="text"
+//           >
+//             {categories.map((categori) => (
+//               <Button key={categori.id}>{categori.title}</Button>
+//             ))}
+//           </ButtonGroup>
+//         </Paper>
+//       </React.Fragment>
+//     </div>
+//   );
+// }
--- client/src/pages/Map/Map.jsx
+++ client/src/pages/Map/Map.jsx
@@ -5,10 +5,35 @@ import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import Card from "../../components/Card";
 import Sidebar from "../../components/Sidebar";
-import MapSidebar from "../../components/MapSidebar";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
 
 export default function Map() {
   const [data, setData] = useState([]);
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      marginTop: 20,
+      background: "#D7E1E9",
+      minHeight: 700,
+      padding: theme.spacing(1),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  }));
+  const classes = useStyles();
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(async () => {
+    const res = await axios("https://alin-ua-api.herokuapp.com/categories");
+
+    setCategories(res.data);
+  }, []);
 
   useEffect(async () => {
     const result = await axios("https://alin-ua-api.herokuapp.com/places");
@@ -21,7 +46,22 @@ export default function Map() {
       <Sidebar />
       <Grid container spacing={0}>
         <Grid container item xs={12} sm={2} md={2} spacing={0}>
-          <MapSidebar />
+          <div className={classes.root}>
+            <>
+              <Paper className={classes.paper}>
+                <ButtonGroup
+                  orientation="vertical"
+                  color="primary"
+                  aria-label="vertical contained primary button group"
+                  variant="text"
+                >
+                  {categories.map((categori) => (
+                    <Button key={categori.id}>{categori.title}</Button>
+                  ))}
+                </ButtonGroup>
+              </Paper>
+            </>
+          </div>
         </Grid>
         <Grid xs={10}>
           <Grid container>
