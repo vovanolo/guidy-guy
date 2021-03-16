1333fcc7d6ce0d3439083201bb882cf6cfc940cb
Vovchik4 <ch.vova321@gmail.com>
2021-03-16 18:20:58 +0200

    PlaceInfo

--- client/src/pages/PlaceInfo/PlaceInfo.jsx
+++ client/src/pages/PlaceInfo/PlaceInfo.jsx
@@ -1,8 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 20,
+    background: "#D7E1E9",
+    minHeight: "100%",
+    maxWidth: 600,
+    //padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    boxShadow: "3px 6px 37px 2px rgba(0, 0, 0, 0.4)",
+    filter: "brightness(70%)",
+  },
+  foto: {
+    width: "100%",
+    height: "100%",
+    filter: "contrast(70%)",
+  },
+  text: {
+    padding: theme.spacing(1),
+  },
+}));
+
 export default function PlaceInfo() {
+  const classes = useStyles();
   const [place, setPlace] = useState([]);
   const { slug } = useParams();
   useEffect(async () => {
@@ -14,9 +44,15 @@ export default function PlaceInfo() {
 
   return (
     <div>
-      {/* <img src={place.photo.url} alt="" /> */}
-      <h1>{place.name}</h1>
-      <h2>{place.desc}</h2>
+      <div className={classes.paper}>
+        <img
+          className={classes.foto}
+          src="https://discover-ukraine.info/uploads/i/i/4ffd2c1051c059.81722594.805.jpg"
+          alt=""
+        />
+        <h1 className={classes.text}>{place.name}</h1>
+        <h2>{place.desc}</h2>
+      </div>
     </div>
   );
 }
